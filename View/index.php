a4c48a2e22ee8f6fe5c8287ab311153818b26b57
Achraf <achrafelmahmoudi@gmail.com>
2017-05-16 12:25:15 +0200

    initial commit

---
+++ View/index.php
@@ -0,0 +1,22 @@
+
+ <!DOCTYPE html>
+<html>
+	<head>
+		<title>La vaiselle du voisin</title>
+		<link rel="stylesheet" href="login.css">
+	</head>
+	<body>
+
+
+		<h1 class="title">La Vaisedlle du Vosin</h1>
+
+
+  	<input type="button" value="Je fais la vaisselle du voisin ! " >
+		<input type="button" value="Je cherche un voisin pour faire ma vaisselle" >
+
+
+
+
+	</body>
+</html>
+**/
