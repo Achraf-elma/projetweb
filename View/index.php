4b55f1fafc2ed780b8e5b0dfcd60e06e2f67edaa
Achraf <achrafelmahmoudi@gmail.com>
2017-05-16 23:20:40 +0200

    update

--- View/index.php
+++ View/index.php
@@ -1,9 +1,9 @@
-
 <?php
 
 	require_once("header.php");
 
 ?>
+
 <html>
   <body>
 
@@ -13,9 +13,9 @@
     <div class ="divButton">
 
         <button class="buttonVaisselle" onclick="javascript:location.href='offres.php'" ><span>Je fais la vaisselle d'un de mes voisins </span></button>
-        <button class="buttonVaisselle" onclick="javascript:location.href='votreannonce.php'" ><span>Je cherche un voisin pour faire ma vaiselle</span></button>
+        <button class="buttonVaisselle" onclick="javascript:location.href='votreannonce.php'"><span>Je cherche un voisin pour faire ma vaiselle</span></button>
     </div>
-        
+
 
 
 
--- View/votreannnonce.php
+++ View/votreannnonce.php
@@ -4,4 +4,15 @@
 
 ?>
 
-// formulaire pour ajouter une annonce
+
+<html>
+  <body>
+
+  <h1 style="center-align"> Postez votre annonce immediatement </h1>
+
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="bootstrap-3/js/bootstrap.min.js"></script>
+  </body>
+</html>
