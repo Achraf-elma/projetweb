ae29d57056eafb715a5eb05cc0a767aafa152f18
Achraf <achrafelmahmoudi@gmail.com>
2017-05-16 17:22:21 +0200

    bouton

--- View/index.php
+++ View/index.php
@@ -12,22 +12,10 @@
 
     <div class ="divButton">
         <h1> Vaisselle du Voisin </h1>
-        <button class="button" ><span>Je fais la vaisselle d'un de mes voisins </span></button>
-        <button class="button" ><span>Je cherche un voisin pour faire ma vaiselle</span></button>
+        <button class="buttonVaisselle" ><span>Je fais la vaisselle d'un de mes voisins </span></button>
+        <button class="buttonVaisselle" ><span>Je cherche un voisin pour faire ma vaiselle</span></button>
     </div>
 
-
-
-<div class="btn-group dropup">
-  <button type="button" class="btn btn-default">Dropup</button>
-  <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    <span class="caret"></span>
-    <span class="sr-only">Toggle Dropdown</span>
-  </button>
-  <ul class="dropdown-menu">
-    <!-- Dropdown menu links -->
-  </ul>
-</div>
 
 
 
