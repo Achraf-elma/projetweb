a055a3aefb009f748f2638fb57fccddfb9dadc4d
Achraf <achrafelmahmoudi@gmail.com>
2017-05-16 22:57:13 +0200

    update

--- View/index.php
+++ View/index.php
@@ -15,6 +15,13 @@
         <button class="buttonVaisselle" onclick="javascript:location.href='offres.php'" ><span>Je fais la vaisselle d'un de mes voisins </span></button>
         <button class="buttonVaisselle" href="votreannonce.php" ><span>Je cherche un voisin pour faire ma vaiselle</span></button>
     </div>
+        <a class="buttonVaisselle" href="offres.php"> ahaha </a>
+				<form method="POST" action="offres.php">
+				<input type="submit" value="google" class="buttonVaisselle">
+				<form action="www.yahoo.fr">
+				<input type="submit" value="yahoo" class="flatButton">
+				</form>
+		</form>
 
 
 
