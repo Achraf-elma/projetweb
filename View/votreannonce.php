1a555affdb8b82353cc39fbdbffdb7f1278ead2a
Achraf <achrafelmahmoudi@gmail.com>
2017-05-16 23:29:22 +0200

    update

---
+++ View/votreannonce.php
@@ -0,0 +1,24 @@
+<?php
+
+	require_once("header.php");
+
+?>
+
+
+<html>
+  <body>
+
+  <h1 style="center-align"> Postez votre annonce immediatement </h1>
+
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="bootstrap-3/js/bootstrap.min.js"></script>
+  </body>
+</html>
+
+<?php
+
+	require_once("footer.php");
+
+?>
