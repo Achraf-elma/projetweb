9d54a0aec7b45a57cbc48dea0d9ef4a42baea9a8
Achraf <achrafelmahmoudi@gmail.com>
2017-05-16 22:54:58 +0200

    update

--- View/index.php
+++ View/index.php
@@ -12,7 +12,7 @@
 
     <div class ="divButton">
 
-        <button class="buttonVaisselle" onclick="javascript:location.href=offres.php" ><span>Je fais la vaisselle d'un de mes voisins </span></button>
+        <button class="buttonVaisselle" onclick="javascript:location.href='offres.php'" ><span>Je fais la vaisselle d'un de mes voisins </span></button>
         <button class="buttonVaisselle" href="votreannonce.php" ><span>Je cherche un voisin pour faire ma vaiselle</span></button>
     </div>
 
