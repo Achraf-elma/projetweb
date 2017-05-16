c05d79dc5f37954588665f3799a1c93e1f0a8a22
Achraf <achrafelmahmoudi@gmail.com>
2017-05-16 16:38:41 +0200

    bouton

---
+++ View/header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <title>La Vaiselle du Voisin</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="bootstrap-3/css/vaisselle.css" rel="stylesheet">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <header><!-- En-tête du site -->
+  <table> <!-- Menu -->
+    <tr> <!-- 1 seule ligne / 8 colonnes -->
+
+      <td><span id="active"><a href="index.htm">Accueil</a></espan></td>
+
+      <td><a href="TP1.htm">TP1</a></td>
+
+      <td><a href="TP2.htm">TP2</a></td>
+
+      <td><a href="TP3.htm">TP3</a></td>
+
+      <td><a href="TP4.htm">TP4</a></td>
+
+      <td><a href="TP5.htm">TP5</a></td>
+
+      <td><a href="conclusion.htm">Conclusion</a></td>
+
+      <td><a href="annexes.htm">Annexes</a></td>
+
+    </tr>
+  </table>
+</header>
