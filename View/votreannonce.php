fc9a4d310390b6d8097c75c67236bb631c0cf974
Achraf <achrafelmahmoudi@gmail.com>
2017-05-17 11:49:28 +0200

    update

--- View/votreannonce.php
+++ View/votreannonce.php
@@ -11,6 +11,27 @@
   <h1 style="center-align"> Postez votre annonce immediatement </h1>
  <p>  Par un soucis d'hygiene une annonce est mise en ligne seulement 48H au dela vous devriez peut etre penser a faire votre propre vaisselle. </p>
 
+ <div class="form-style-3">
+<form>
+<fieldset><legend>Personal</legend>
+<label for="prix"><span>Prix <span class="required">*</span></span><input type="text" class="input-field" name="prix" value="" /></label>
+<label for="field2"><span>Email <span class="required">*</span></span><input type="email" class="input-field" name="field2" value="" /></label>
+<label for="field3"><span>Phone <span class="required">*</span></span><input type="text" class="input-field" name="field3" value="" /></label>
+<label for="field4"><span>Quantite de Vaisselle</span><select name="field4" class="select-field">
+																				<option value="Grosse">Grosse</option>
+																				<option value="Moyenne">Moyenne</option>
+																				<option value="Petite">Petite</option>
+</select></label>
+</fieldset>
+<fieldset><legend>Commentaire</legend>
+<label for="field6"><span>Message <span class="required">*</span></span><textarea name="field6" class="textarea-field"></textarea></label>
+<label><span>&nbsp;</span><input type="submit" value="Submit" /></label>
+</fieldset>
+</form>
+</div>
+
+
+
 	<form method="post" action="/Controller/controller_annonce_validation.php">
 
 		<input type="text" placeholder="Prix (Maximum 13 euros)" name="id"><br>
