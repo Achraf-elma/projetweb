87b047fb7c1bb8fac7f7fa36b2b36fc94099007e
Achraf <achrafelmahmoudi@gmail.com>
2017-05-17 16:39:59 +0200

    update

--- Controller/controller_votreannonce.php
+++ Controller/controller_votreannonce.php
@@ -7,8 +7,5 @@
     $Quantite=$_POST['Quantite'];
     $mess=$_POST['message'];
 
-    if (empty($ville) || empty($quartier) || empty($mdp))
-		{
-			header("Location: ../erreur.php?message=Merci_de_complèter_tous_vos_champs");
-		}
+    echo $ville;
  ?>
