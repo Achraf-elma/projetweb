8c29ef1d6063bc53f6234c4495bc216e2cac8b8a
Achraf <achrafelmahmoudi@gmail.com>
2017-05-17 22:27:59 +0200

    update

--- View/connexion.php
+++ View/connexion.php
@@ -7,18 +7,15 @@
 <html>
 
 	<body>
-		 <a href="index.php"><img src="View/medias/polytech.jpg" height="100" class="logo"></a>
-
 		<h1>Connexion</h1>
 
-		<form method="post" action="/Controller/controller_connexion.php">
+    <form method="post" action="/Controller/controller_connexion.php">
 
 			<input type="text" placeholder="Pseudo" name="id"><br>
 			<input type="password" placeholder="Mot de passe" name="mdp"><br>
-
 			<input type="submit" value="Connexion" class="connectionButton"><br>
 
-			<p>Cette page concerne les élèves de Polytech.</p>
+       <p>Cette page concerne les élèves de Polytech.</p>
 
 		</form>
 
