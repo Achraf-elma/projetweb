e148c18e4be9ba7687057349968ac10847ea3e9c
Achraf <achrafelmahmoudi@gmail.com>
2017-05-17 12:15:43 +0200

    update

--- View/votreannonce.php
+++ View/votreannonce.php
@@ -13,6 +13,26 @@
   <body>
 <div style="center-align; margin-left:20%; margin-right:20%;"
 
+
+<h1>Elegant Contact Form</h1>
+<form class="cf">
+  <div class="half left cf">
+    <input type="text" id="input-name" placeholder="Name">
+    <input type="email" id="input-email" placeholder="Email address">
+    <input type="text" id="input-subject" placeholder="Subject">
+  </div>
+  <div class="half right cf">
+    <textarea name="message" type="text" id="input-message" placeholder="Message"></textarea>
+  </div>
+  <input type="submit" value="Submit" id="input-submit">
+</form>
+
+
+
+
+
+
+
  <div class="form-style-3">
 <form>
 <fieldset><legend>Information Personnelle</legend>
