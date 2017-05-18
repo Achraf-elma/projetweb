35566cb8069f5d9c2ded72b51a527623c3c42210
Achraf <achrafelmahmoudi@gmail.com>
2017-05-18 13:13:57 +0200

    update

--- Model/pdo.php
+++ Model/pdo.php
@@ -6,16 +6,6 @@ require('vendor/autoload.php');
 $app = new Silex\Application();
 $app['debug'] = true;
 
-// Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
-));
-
-// Register the Twig templating engine
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-  'twig.path' => __DIR__.'/../views',
-));
-
 // Register the Postgres database add-on
 $dbopts = parse_url(getenv('DATABASE_URL'));
 $app->register(new Herrera\Pdo\PdoServiceProvider(),
@@ -27,36 +17,8 @@ $app->register(new Herrera\Pdo\PdoServiceProvider(),
   )
 );
  echo $dbopts["user"];
-// Our web handlers
 
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return 'Hello';
-});
-
-$app->get('/db/', function() use($app) {
-  $st = $app['pdo']->prepare('SELECT name FROM test_table');
-  $st->execute();
-
-  $names = array();
-  while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
-    $app['monolog']->addDebug('Row ' . $row['name']);
-    $names[] = $row;
-  }
-
-  return $app['twig']->render('database.twig', array(
-    'names' => $names
-  ));
-});
-
-$app->get('/twig/{name}', function($name) use($app) {
-  return $app['twig']->render('index.twig', array(
-    'name' => $name,
-  ));
-});
-
-
-$app->run();
+//$app->run();
 
 
 
