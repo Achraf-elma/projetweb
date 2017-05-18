df32066b2d5d888b926c91961087e37b9462cdba
Achraf <achrafelmahmoudi@gmail.com>
2017-05-18 16:28:57 +0200

    up

--- Model/membre.php
+++ Model/membre.php
@@ -15,7 +15,7 @@
     }
     */
 echo "ajoutemembre1";
-	function ajoutMembre($pseudo, $email,$pass_hache, $nom, $prenom, $sexe, $image)
+	/*function ajoutMembre($pseudo, $email,$pass_hache, $nom, $prenom, $sexe, $image)
 	#Donnée: Email (chaine de char) de l'étudiant, un mdp haché, nom(char) et prénom(char) de l'étudiant ainsi que sa promo (int = idPromo)
 	#Post: ajoute l'étudiant à la base de données
 	{
@@ -27,6 +27,6 @@ echo "ajoutemembre1";
         $ajout->execute();
         pg_close($bd);
 	}
-
+*/
 
 ?>
