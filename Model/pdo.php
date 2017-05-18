7b96d281e1b74d1af06cc4241c429ade6f74a22f
Achraf <achrafelmahmoudi@gmail.com>
2017-05-18 13:50:47 +0200

    update

--- Model/pdo.php
+++ Model/pdo.php
@@ -6,8 +6,9 @@ function connexion(){
  $dbname = "d45kt38b1qptd2";
  $user = "kjkoobypuqkyls";
  $psw = "efda74dc519f256390b7ff5edada82bab39005467f833909383fb5d9bd15dcb3"
-  $dbconn = pg_connect("host='".$host."' dbname='".$dbname."' user='".$user."' password='".$pwd."'")
-   or die('Connexion impossible : ' . pg_last_error());
+
+  $dbconn = pg_connect("host=ec2-184-73-236-170.compute-1.amazonaws.com dbname=d45kt38b1qptd2 user=kjkoobypuqkyls password=efda74dc519f256390b7ff5edada82bab39005467f833909383fb5d9bd15dcb3")
+      or die('Connexion impossible : ' . pg_last_error());
 
    return $dbconn;
 }
