ae383b3d4039f68cf71232a746259c540af60ace
Achraf <achrafelmahmoudi@gmail.com>
2017-05-18 13:54:58 +0200

    update

--- Controller/controller_page_index.php
+++ Controller/controller_page_index.php
@@ -2,7 +2,7 @@
 
   echo "on y est";
 
-require_once("Model/pdo.php")
+
 // Connexion, sélection de la base de données
 $dbconn = pg_connect("host=ec2-184-73-236-170.compute-1.amazonaws.com dbname=d45kt38b1qptd2 user=kjkoobypuqkyls password=efda74dc519f256390b7ff5edada82bab39005467f833909383fb5d9bd15dcb3")
     or die('Connexion impossible : ' . pg_last_error());
