63778cb9f5817920c3c3969b94d4c45c0f71ef14
Achraf <achrafelmahmoudi@gmail.com>
2017-05-19 00:23:25 +0200

    up

--- View/header.php
+++ View/header.php
@@ -26,12 +26,13 @@
    <?php
     if (isset($_COOKIE['id']))
     {
-      echo "<li class="right"><a href="inscription.php">Inscription</a></li>
-    <li class="right"><a href="connexion.php">Connexion</a></li>"; }
-    else {
-      echo "<li class="right"><a href="deconnexion.php">Deconnexion</a></li>";
+      echo '<li class="right"><a href="inscription.php">Inscription</a></li>
+    <li class="right"><a href="connexion.php">Connexion</a></li>';
     }
-    ?>;
+    else {
+      echo '<li class="right"><a href="deconnexion.php">Deconnexion</a></li>';
+    }
+    ?>
 
   <!--  <li class="right"><a href="#about">Mon profil</a></li> -->
   </ul>
