e3b0b73f1f26576a59ab0914f825f069f22f26dc
Achraf <achrafelmahmoudi@gmail.com>
2017-05-18 12:12:04 +0200

    update

--- Controller/controller_page_index.php
+++ Controller/controller_page_index.php
@@ -3,8 +3,8 @@
   echo "on y est";
   require_once("../Model/pdo.php");
   echo "on a";
-  $bd = connexion();
-  if (!empty($bd)){
+/*  $bd = connexion();
+  /* if (!empty($bd)){
 	   require_once("View/index.php");
-    }
+   } */
 ?>
--- Model/pdo.php
+++ Model/pdo.php
@@ -1,23 +1,5 @@
 <?php
 echo "on est dans le pdo";
-function connexion(){
-echo "on est dans la fonction";
-    try {
-      $dbopts = parse_url(getenv('postgres://kjkoobypuqkyls:efda74dc519f256390b7ff5edada82bab39005467f833909383fb5d9bd15dcb3@ec2-184-73-236-170.compute-1.amazonaws.com:5432/d45kt38b1qptd2'));
-      $app->register(new Herrera\Pdo\PdoServiceProvider(),
-                     array(
-                         'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"] . ';port=' . $dbopts["port"],
-                         'pdo.username' => $dbopts["user"],
-                         'pdo.password' => $dbopts["pass"]
-                     )
-    }
-    catch(PDOException $e) {
-      echo "erreur";
-      $dbopts = null;
-      echo 'ERREUR DB: ' . $e->getMessage();
-    }
 
-   return ($dbopts);
-}
 
 ?>
