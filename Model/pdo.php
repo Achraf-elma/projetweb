f2ed553ca5cf09cef4e26755f4a0cf75bf07b42a
Achraf <achrafelmahmoudi@gmail.com>
2017-05-18 17:33:58 +0200

    up

--- Model/pdo.php
+++ Model/pdo.php
@@ -2,8 +2,8 @@
 
 function connexion(){
 
-
- /* Hiroku data
+/*
+  Hiroku data
  ------------------------------------------
   $host = "ec2-184-73-236-170.compute-1.amazonaws.com";
   $dbname = "d45kt38b1qptd2";
@@ -13,14 +13,16 @@ function connexion(){
 
  $buffer ="host=ec2-184-73-236-170.compute-1.amazonaws.com dbname=d45kt38b1qptd2 user=kjkoobypuqkyls password=efda74dc519f256390b7ff5edada82bab39005467f833909383fb5d9bd15dcb3";
  $dbconn = pg_connect($buffer)  or die('Connexion impossible : ' . pg_last_error());
- */
+
 
  $dsn = "ec2-184-73-236-170.compute-1.amazonaws.com;dbname=d45kt38b1qptd2";
  $login = "kjkoobypuqkyls";
-  $mdp = "efda74dc519f256390b7ff5edada82bab39005467f833909383fb5d9bd15dcb3";
- echo "yes";
+  $mdp = "efda74dc519f256390b7ff5edada82bab39005467f833909383fb5d9bd15dcb3"; */
+
+
   // Création de la connexion
-  $bd = new PDO($dsn, $login, $mdp);
+  $bd = new PDO('pgsql:host=ec2-184-73-236-170.compute-1.amazonaws.com;port=5432;dbname=d45kt38b1qptd2;user=kjkoobypuqkyls;password=efda74dc519f256390b7ff5edada82bab39005467f833909383fb5d9bd15dcb3');
+  echo "yes";
  return $bd;
 
 }
