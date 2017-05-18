73f9af934d96715475c043b39d27daf2c88e1af8
Achraf <achrafelmahmoudi@gmail.com>
2017-05-18 12:59:47 +0200

    update

--- Model/pdo.php
+++ Model/pdo.php
@@ -1,23 +1,66 @@
 <?php
 echo "ok";
 
+
+function connexion()
+  {
 require('../vendor/autoload.php');
+
 $app = new Silex\Application();
 $app['debug'] = true;
 
+// Register the monolog logging service
+$app->register(new Silex\Provider\MonologServiceProvider(), array(
+  'monolog.logfile' => 'php://stderr',
+));
 
- function connexion(){
+// Register the Twig templating engine
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+  'twig.path' => __DIR__.'/../views',
+));
 
- $dbopts = parse_url(getenv('DATABASE_URL'));
- $app->register(new Herrera\Pdo\PdoServiceProvider(),
-   array(
-     'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"],
-     'pdo.port' => $dbopts["port"],
-     'pdo.username' => $dbopts["user"],
-     'pdo.password' => $dbopts["pass"]
-   )
- );
-return ($dbopts);
+// Register the Postgres database add-on
+$dbopts = parse_url(getenv('DATABASE_URL'));
+$app->register(new Herrera\Pdo\PdoServiceProvider(),
+  array(
+    'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"],
+    'pdo.port' => $dbopts["port"],
+    'pdo.username' => $dbopts["user"],
+    'pdo.password' => $dbopts["pass"]
+  )
+);
+
+// Our web handlers
+
+$app->get('/', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return 'Hello';
+});
+
+$app->get('/db/', function() use($app) {
+  $st = $app['pdo']->prepare('SELECT name FROM test_table');
+  $st->execute();
+
+  $names = array();
+  while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
+    $app['monolog']->addDebug('Row ' . $row['name']);
+    $names[] = $row;
+  }
+
+  return $app['twig']->render('database.twig', array(
+    'names' => $names
+  ));
+});
+
+$app->get('/twig/{name}', function($name) use($app) {
+  return $app['twig']->render('index.twig', array(
+    'name' => $name,
+  ));
+});
+
+
+$app->run();
+
+return $dbopts;
 }
-echo "ok2";
 ?>
