3dd0d6a9c30c2a9e9df694a961a7d2f1555559ea
Achraf <achrafelmahmoudi@gmail.com>
2017-05-18 12:29:55 +0200

    update

--- Model/pdo.php
+++ Model/pdo.php
@@ -4,7 +4,7 @@ echo "ok";
 
  function connexion(){
  echo "on est dans la fonction";
-        try{
+      
           $dbopts = parse_url(getenv('postgres://kjkoobypuqkyls:efda74dc519f256390b7ff5edada82bab39005467f833909383fb5d9bd15dcb3@ec2-184-73-236-170.compute-1.amazonaws.com:5432/d45kt38b1qptd2'));
           $app->register(new Herrera\Pdo\PdoServiceProvider(),
                array(
@@ -12,12 +12,8 @@ echo "ok";
                    'pdo.username' => $dbopts["user"],
                    'pdo.password' => $dbopts["pass"]
                )
-);
-        }
-        catch(Exception $e)
-        {
-            die('<br> Echec lors de la connexion la BD :'.$e->getMessage());
-        }
+             );
+
 
        return ($dbopts);
 
