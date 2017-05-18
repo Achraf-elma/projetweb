d3570e694e3d37f5c7cec6c37005a266ade9cf33
Achraf <achrafelmahmoudi@gmail.com>
2017-05-18 13:57:19 +0200

    update

--- Controller/controller_page_index.php
+++ Controller/controller_page_index.php
@@ -28,6 +28,13 @@ pg_free_result($result);
 // Ferme la connexion
 pg_close($dbconn);
 
+$db = pg_connect("host=ec2-184-73-236-170.compute-1.amazonaws.com dbname=d45kt38b1qptd2 user=kjkoobypuqkyls password=efda74dc519f256390b7ff5edada82bab39005467f833909383fb5d9bd15dcb3")
+    or die('Connexion impossible : ' . pg_last_error());
+$qry = $db->prepare("select nom from membres;");
+$qry->execute();
+
+$noms = $qry->fetchAll();
+print_r($noms);
 
 
 
