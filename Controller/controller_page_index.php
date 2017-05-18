5a14ab3fac028fac6d6f09b2e3b8b9ab365a78a4
Achraf <achrafelmahmoudi@gmail.com>
2017-05-18 13:52:59 +0200

    update

--- Controller/controller_page_index.php
+++ Controller/controller_page_index.php
@@ -4,7 +4,8 @@
 
 require_once("Model/pdo.php")
 // Connexion, sélection de la base de données
-$dbconn = connexion();
+$dbconn = pg_connect("host=ec2-184-73-236-170.compute-1.amazonaws.com dbname=d45kt38b1qptd2 user=kjkoobypuqkyls password=efda74dc519f256390b7ff5edada82bab39005467f833909383fb5d9bd15dcb3")
+    or die('Connexion impossible : ' . pg_last_error());
 
 // Exécution de la requête SQL
 $query = 'SELECT * FROM membre';
