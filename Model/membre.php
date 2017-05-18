5fd2fed05bab143be8de69fc2ee8bcb8bab4f4d0
Achraf <achrafelmahmoudi@gmail.com>
2017-05-18 16:22:52 +0200

    up

--- Model/membre.php
+++ Model/membre.php
@@ -14,15 +14,15 @@
         return $mdpBD;
     }
     */
-
+echo "ajoutemembre1";
 	function ajoutMembre($pseudo, $email,$pass_hache, $nom, $prenom, $sexe, $image)
 	#Donnée: Email (chaine de char) de l'étudiant, un mdp haché, nom(char) et prénom(char) de l'étudiant ainsi que sa promo (int = idPromo)
 	#Post: ajoute l'étudiant à la base de données
 	{
-	      require_once("pdo.php");
-						echo "ajoutemembre";
+	      require_once("Model/pdo.php");
+						echo "ajoutemembre2";
         $bd = connexion();
-				echo "ajoutemembre";
+				echo "ajoutemembre3";
         $ajout = prepare( "INSERT INTO etudiant (pseudo, email, mdp_membre, nom, prenom, sexe, image) VALUES ('".$pseudo."','".$email."','".$pass_hache."','".$nom."','".$prenom."','".$sexe."', '".$image."')");
         $ajout->execute();
         pg_close($bd);
