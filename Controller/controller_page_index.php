8aa52b78c7276ab1dbaa7ba2e4c08e3f142e885c
Achraf <achrafelmahmoudi@gmail.com>
2017-05-18 14:21:18 +0200

    update

--- Controller/controller_page_index.php
+++ Controller/controller_page_index.php
@@ -1,32 +1,9 @@
 <?php
 
-  echo "on y est";
-
-require_once("Model/pdo.php");
-// Connexion, sélection de la base de données
-$dbconn = connexion();
 
 
-// Exécution de la requête SQL
-$query = 'SELECT * FROM membre';
-$result = pg_query($query) or die('Échec de la requête : ' . pg_last_error());
+require_once("View/index.php");
 
-// Affichage des résultats en HTML
-echo "<table>\n";
-while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
-    echo "\t<tr>\n";
-    foreach ($line as $col_value) {
-        echo "\t\t<td>$col_value</td>\n";
-    }
-    echo "\t</tr>\n";
-}
-echo "</table>\n";
-
-// Libère le résultat
-pg_free_result($result);
-
-// Ferme la connexion
-pg_close($dbconn);
 
 
 
---
+++ Model/membre.php
@@ -0,0 +1,29 @@
+<?php
+
+	/* function verifMdp($pseudo)
+	#Donnnée: pseudo de l'étudiant connecté
+	#Résultat: Renvoie le mot de passe de l'étudiant
+    {
+        require_once("pdo.php");
+
+        $bd = connexion();
+        $req = $bd->prepare("SELECT mdp_membre FROM membre WHERE pseudo='".$pseudo."'");
+        $req->execute();
+        $mdpBD=$req->fetch();
+		    $req->closeCursor();
+        return $mdpBD;
+    }
+    */  
+	function ajoutMembre($pseudo, $email,$pass_hache, $nom, $prenom, $promo)
+	#Donnée: Email (chaine de char) de l'étudiant, un mdp haché, nom(char) et prénom(char) de l'étudiant ainsi que sa promo (int = idPromo)
+	#Post: ajoute l'étudiant à la base de données
+	{
+	      require_once("pdo.php");
+        $bd = connexion();
+        $ajout = $bd->prepare( "INSERT INTO etudiant (pseudo, email, mdp_membre, nom, prenom, sexe, image) VALUES ('".$pseudo."','".$email."','".$pass_hache."','".$nom."','".$prenom."','".$sexe."', '".$image."',)");
+        $ajout->execute();
+        pg_close($bd);
+	}
+
+
+?>
