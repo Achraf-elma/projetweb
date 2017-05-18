dce03c63ca9247b92377cd7bec93dc42402fff4b
Achraf <achrafelmahmoudi@gmail.com>
2017-05-18 12:09:09 +0200

    update

--- Controller/controller_page_index.php
+++ Controller/controller_page_index.php
@@ -1,7 +1,8 @@
 <?php
 
-
-  require_once("Model/pdo.php");
+  echo "on y est";
+  require_once("../Model/pdo.php");
+  echo "on a";
   $bd = connexion();
   if (!empty($bd)){
 	   require_once("View/index.php");
