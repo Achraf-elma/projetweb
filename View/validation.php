84c0bf46527611ca340c220d3888ea7b94ee478e
Achraf <achrafelmahmoudi@gmail.com>
2017-05-18 22:04:20 +0200

    up

--- View/validation.php
+++ View/validation.php
@@ -2,18 +2,16 @@
 <html>
 	<head>
 		<title>Validation</title>
-		<link rel="stylesheet" href="View/login.css">
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	</head>
- <a href="index.php"><img class=polytech src="View/medias/polytech.jpg" alt="Polytech Montpellier" title="Cliquez pour agrandir" width="280" height="80"/></a>
-	<body>
-
-		<h1 class="title">Vous êtes connecté</h1>
 
 
+		<h1 class="title">Vous êtes inscrit</h1>
 
+		<p>  <?php echo $message ?>   </p>
+    <a  class="buttonVaisselle" type="button"  href="index.php"> Retour</a>
 		<div style="text-align:center">
-		<a  href=<?php echo "\"".$lien.".php\"" ?> > <input class="connectionButton" type="button" value="Lancer le test">
+
 		</div>
 
 	</body>
