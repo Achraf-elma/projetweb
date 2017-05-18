e0df10f77d41d95e758906b25fc9e22cefc91ade
Achraf <achrafelmahmoudi@gmail.com>
2017-05-18 12:54:17 +0200

    update

--- Model/pdo.php
+++ Model/pdo.php
@@ -1,22 +1,23 @@
 <?php
 echo "ok";
 
+require('../vendor/autoload.php');
+$app = new Silex\Application();
+$app['debug'] = true;
+
 
  function connexion(){
- echo "on est dans la fonction";
 
-          $dbopts = parse_url(getenv('postgres://kjkoobypuqkyls:efda74dc519f256390b7ff5edada82bab39005467f833909383fb5d9bd15dcb3@ec2-184-73-236-170.compute-1.amazonaws.com:5432/d45kt38b1qptd2'));
-          $app->register(new Herrera\Pdo\PdoServiceProvider(),
-               array(
-                   'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"] . ';port=' . $dbopts["port"],
-                   'pdo.username' => $dbopts["user"],
-                   'pdo.password' => $dbopts["pass"]
-               )
-             );
-
-
-       return ($dbopts);
-
+ $dbopts = parse_url(getenv('DATABASE_URL'));
+ $app->register(new Herrera\Pdo\PdoServiceProvider(),
+   array(
+     'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"],
+     'pdo.port' => $dbopts["port"],
+     'pdo.username' => $dbopts["user"],
+     'pdo.password' => $dbopts["pass"]
+   )
+ );
+return ($dbopts);
 }
 echo "ok2";
 ?>
