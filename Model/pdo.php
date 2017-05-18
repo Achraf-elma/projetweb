ffad98505f4aeac51a3ae53d2ff3e9b24bf9c12a
Achraf <achrafelmahmoudi@gmail.com>
2017-05-18 12:20:59 +0200

    update

--- Model/pdo.php
+++ Model/pdo.php
@@ -2,8 +2,8 @@
 echo "ok";
 
 
-/* function connexion(){
-    echo "on est dans la fonction";
+ function connexion(){
+  /*  echo "on est dans la fonction";
         try {
           $dbopts = parse_url(getenv('postgres://kjkoobypuqkyls:efda74dc519f256390b7ff5edada82bab39005467f833909383fb5d9bd15dcb3@ec2-184-73-236-170.compute-1.amazonaws.com:5432/d45kt38b1qptd2'));
           $app->register(new Herreraecho "ok";\Pdo\PdoServiceProvider(),
@@ -20,6 +20,7 @@ echo "ok";
         }
 
        return ($dbopts);
-} */
+       */
+}
 echo "ok2";
 ?>
