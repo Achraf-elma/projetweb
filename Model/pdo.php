3f6a168520f0c5bbb830bf65ff0d9dde029a3da8
Achraf <achrafelmahmoudi@gmail.com>
2017-05-18 12:38:01 +0200

    update

--- Model/pdo.php
+++ Model/pdo.php
@@ -3,20 +3,25 @@ echo "ok";
 
 
  function connexion(){
- echo "on est dans la fonction";
+   use Herrera\Pdo\PdoServiceProvider;
+   use Silex\Application;
 
-          $dbopts = parse_url(getenv('postgres://kjkoobypuqkyls:efda74dc519f256390b7ff5edada82bab39005467f833909383fb5d9bd15dcb3@ec2-184-73-236-170.compute-1.amazonaws.com:5432/d45kt38b1qptd2'));
-          $app->register(new Herrera\Pdo\silex-pdo(),
-               array(
-                   'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"] . ';port=' . $dbopts["port"],
-                   'pdo.username' => $dbopts["user"],
-                   'pdo.password' => $dbopts["pass"]
-               )
-             );
+ $app = new Application();
+ $app->register(
+     new PdoServiceProvider(),
+     array(
+         'pdo.dsn' => 'pdo_pgsql:dbname=d45kt38b1qptd2;host=ec2-184-73-236-170.compute-1.amazonaws.com',
+         'pdo.username' => 'username', // optional
+         'pdo.password' => 'password', // optional
+         'pdo.options' => array( // optional
+          //   PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'"
+         )
+     )
+ );
 
+ $pdo = $app['pdo'];
 
-       return ($dbopts);
-
+ return pdo;
 }
 echo "ok2";
 ?>
