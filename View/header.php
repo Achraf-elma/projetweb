949949eea3e6e17a452f29ab2589aae3297b2fef
Achraf <achrafelmahmoudi@gmail.com>
2017-05-19 13:45:00 +0200

    up

--- View/header.php
+++ View/header.php
@@ -13,10 +13,11 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+  <a href="index.php" > <img src="./View/image/banniere.jpg" width="640" height="200"> </a>
   </head>
 
   <header><!-- En-tête du site -->
-  <a href="index.php" > <img src="./View/image/banniere.jpg" width="640" height="200"> </a>
+
   <ul class="topnav">
 
     <li><a class="active" href="#home">Accueil</a></li>
