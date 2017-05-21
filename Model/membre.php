e1279d226d5e7b9d537c781fd55e764c28638ac2
Achraf <achrafelmahmoudi@gmail.com>
2017-05-21 11:55:05 +0200

    up

--- Model/membre.php
+++ Model/membre.php
@@ -18,12 +18,13 @@
 	#Donnée: Email (chaine de char) de l'étudiant, un mdp haché, nom(char) et prénom(char) de l'étudiant ainsi que sa promo (int = idPromo)
 	#Post: ajoute l'étudiant à la base de données
 	{
-		echo "done";
+
 
 	   require_once("../Model/pdo.php");
 
 		 require_once("../Model/quartier.php");
 		 require_once("../Model/ville.php");
+		 echo "super";
      $bd = connexion();
 
 		// On creer la ville / quartier si besoin. ET on recupere leur id respectif
