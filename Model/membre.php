79714aeeaa2f5dc99f6152900cd4415b9ed72390
Achraf <achrafelmahmoudi@gmail.com>
2017-05-21 11:27:37 +0200

    up

--- Model/membre.php
+++ Model/membre.php
@@ -19,19 +19,20 @@
 	#Post: ajoute l'étudiant à la base de données
 	{
 		echo "done";
+		/*
 	   require_once("../Model/pdo.php");
 		 require_once("../Model/quartier.php");
 		 require_once("../Model/ville.php");
      $bd = connexion();
-
+   */
 		// On creer la ville / quartier si besoin. ET on recupere leur id respectif
-		 creerVilleIfNotExists($nomVille);
+		 /* creerVilleIfNotExists($nomVille);
 		 creerQuartierIfNotExists($nomQuartier);
 		 $idville = recupIdVille($nomVille));
 		 $idquartier = recupIdQuartier($nomQuartier);
 
         $ajout = $bd->prepare( "INSERT INTO membre(pseudo, email, mdp_membre, telephone, sexe, ville, quartier) VALUES ('".$pseudo."','".$email."','".$pass_hache."','".$telephone."','".$prenom."','".$sexe."', '".$ville."', '".$quartier."')");
-        $ajout->execute();
+        $ajout->execute(); */
 
 	}
 
