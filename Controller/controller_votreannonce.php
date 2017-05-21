adb743af2f3598d01404322f293e462a52eee027
Achraf <achrafelmahmoudi@gmail.com>
2017-05-21 16:51:37 +0200

    up

--- Controller/controller_votreannonce.php
+++ Controller/controller_votreannonce.php
@@ -1,6 +1,6 @@
 <?php
 
-
+    require_once("../Model/offres.php");
     $prix=!empty($_POST["prix"]) ? $_POST['prix'] : NULL;
     $quantite=!empty($_POST["quantite"]) ? $_POST["quantite"] : NULL;
     $commentaire=!empty($_POST["message"]) ? $_POST["message"] : NULL;
@@ -16,7 +16,7 @@
 		else
 		{
 
-			ajoutOffre($prix,$quantiteVaisselle,$cookie,$commentaire);
+			creerOffre($prix,$quantiteVaisselle,$cookie,$commentaire);
 
 		//	header("Location: ../validation.php?message=Votre_annonce_est_poste");
 		}
