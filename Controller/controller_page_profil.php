9c70d456c1c93ffca6e15cb5aaec3dceb64893f1
Achraf <achrafelmahmoudi@gmail.com>
2017-05-24 17:05:56 +0200

    up

--- Controller/controller_page_profil.php
+++ Controller/controller_page_profil.php
@@ -3,7 +3,8 @@
 
 require_once("Model/membre.php");
   //echo $_POST[id];
-$idprofil ="Admin";
+
+  $idprofil ="Admin";
   $answer=dataMembre($idprofil);
   echo $idprofil;
 	require_once("View/profil.php");
