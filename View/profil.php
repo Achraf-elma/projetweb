5e745e83775d85b9c2e366a277a990717c80e4b7
Achraf <achrafelmahmoudi@gmail.com>
2017-05-24 17:16:37 +0200

    up

--- View/profil.php
+++ View/profil.php
@@ -3,17 +3,14 @@
 	require_once("header.php");
 
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-    <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 
-
-    </head>
     <body>
 
 
- Profil de <?php echo $membre['pseudo']; ?>
+ <h1> Profil de <?php echo $membre['pseudo']; ?> </h1>
+<div class=profil>
+
+
 
 
        <script type="text/javascript" src="./View/afficher_cacher_div.js"></script>
@@ -28,10 +25,7 @@
     </script>
 
 
-    </body>
-</html>
-
-
+</body>
 
 <?php
 
