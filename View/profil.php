406d36d258963bf1e59af91b0676c1e074f1fd04
Achraf <achrafelmahmoudi@gmail.com>
2017-05-24 16:53:16 +0200

    up

--- View/profil.php
+++ View/profil.php
@@ -17,18 +17,6 @@
  Profil de <?php echo $donnees["pseudo"]; ?>
 
 
-       <script type="text/javascript" src="./View/afficher_cacher_div.js"></script>
-    <span class="bouton" id="bouton_texte" onclick="javascript:afficher_cacher('texte');">Afficher le texte</span>
-    <div style="text-align:center; width:10%;" id="texte" class="texte">
-        <?php echo $donnees["telephone"]; ?>
-    </div>
-    <script type="text/javascript">
-    //<!--
-        afficher_cacher('texte');
-    //-->
-    </script>
-
-
 
 
 		<?php
