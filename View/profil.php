abd246b530aba2a88452c9873b5fb7347c5d3c55
Achraf <achrafelmahmoudi@gmail.com>
2017-05-24 16:51:26 +0200

    up

--- View/profil.php
+++ View/profil.php
@@ -6,15 +6,14 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 
     <script type="text/javascript" src="./View/afficher_cacher_div.js"></script>
     </head>
     <body>
 
-			<?php while($donnees = $answer->fetch())
-			{
-				?>
+			<?php while($donnees = $answer->fetch()) {
+		  ?>
  Profil de <?php echo $donnees["pseudo"]; ?>
 
 
