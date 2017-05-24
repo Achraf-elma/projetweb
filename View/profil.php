6cd78da58b15d1d0db7c8b6adf511e8ee0abaf70
Achraf <achrafelmahmoudi@gmail.com>
2017-05-24 16:17:30 +0200

    up

--- View/profil.php
+++ View/profil.php
@@ -6,14 +6,15 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>Afficher / Cacher une boite div en javascript</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />>
 
     <script type="text/javascript" src="./View/afficher_cacher_div.js"></script>
     </head>
     <body>
+
+
     <span class="bouton" id="bouton_texte" onclick="javascript:afficher_cacher('texte');">Afficher le texte</span>
-    <div id="texte" class="texte">
+    <div style="text-align:center" id="texte" class="texte">
         0651293170
     </div>
     <script type="text/javascript">
