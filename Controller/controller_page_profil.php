2a6b2e669580fa68763d61adaa87ee12b4edfdf3
Achraf <achrafelmahmoudi@gmail.com>
2017-05-24 16:59:54 +0200

    up

--- Controller/controller_page_profil.php
+++ Controller/controller_page_profil.php
@@ -8,4 +8,24 @@ $idprofil ="Admin";
   echo $idprofil;
 	require_once("View/profil.php");
 
+
+
+
+/*
+
+
+       <script type="text/javascript" src="./View/afficher_cacher_div.js"></script>
+    <span class="bouton" id="bouton_texte" onclick="javascript:afficher_cacher('texte');">Afficher le texte</span>
+    <div style="text-align:center; width:10%;" id="texte" class="texte">
+        <?php echo $donnees["telephone"]; ?>
+    </div>
+    <script type="text/javascript">
+    //<!--
+        afficher_cacher('texte');
+    //-->
+    </script>
+
+
+
+*/
 ?>
--- View/profil.php
+++ View/profil.php
@@ -3,18 +3,16 @@
 	require_once("header.php");
 
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-    <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 
 
-    </head>
+
     <body>
 
 			<?php while($donnees = $answer->fetch()) {
 		  ?>
- Profil de <?php echo $donnees["pseudo"]; ?>
+ Profil de
+
+  <?php echo $donnees["pseudo"]; ?>
 
 
 
@@ -24,7 +22,6 @@
 		$answer->closeCursor();
 		?>
     </body>
-</html>
 
 
 
