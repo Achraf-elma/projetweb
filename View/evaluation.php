0a3a88f06bda1b7136b9584222724c486e94aa02
Achraf <achrafelmahmoudi@gmail.com>
2017-05-24 20:16:40 +0200

    up

--- View/evaluation.php
+++ View/evaluation.php
@@ -1,8 +1,9 @@
 
     <div style="text-align:center;" >
 
-  <?php echo $membre['pseudo']; ?>
-    <form method="post" action="../Controller/controller_evaluation.php?id=">
+  <?php echo $membre['pseudo'];
+  $ps = $membre['pseudo']; ?>
+    <form method="post" <?php echo "action='../Controller/controller_evaluation.php?id=$ps'">  ?>
 
       AVIS SUR CE MEMBRE :
       <input type="radio" name= "note" value="Positif"> Positif
