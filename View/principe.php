01559e16b957d8b1a5ef326e7420365deded597f
Achraf <achrafelmahmoudi@gmail.com>
2017-05-26 01:37:50 +0200

    up

---
+++ View/principe.php
@@ -0,0 +1,15 @@
+<?php
+    require_once("header.php");
+ ?>
+
+<body>
+  <div class="contenu">
+		<h2> CONCEPT </h2>
+<p> Le blablacar des alergiques à la vaisselle. Trouver immediatement quelqu'un de votre quartier pour faire votre vaiselle. Ou bien faites la vaisselle de quelqu'un de votre quartier. Tout cela en échange d'argent bien sur. <br> Grâce à notre systeme de notation et de profil, faites connaissances en toute sérénité avec vos voisins ... et leur vaisselle ! </p>
+  </div>
+</body>
+
+
+<?php
+	    require_once("footer.php");
+?>
