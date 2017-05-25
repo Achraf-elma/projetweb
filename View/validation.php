46f5aa33c9bf48bcaaaba6a77823003dc8b5feea
Achraf <achrafelmahmoudi@gmail.com>
2017-05-26 01:20:20 +0200

    up

--- View/validation.php
+++ View/validation.php
@@ -3,8 +3,10 @@
  ?>
 
 <body>
-		<h2 style="align-text:center;">  <?php echo $message ?>   </h2>
-    <a  class="buttonVaisselle" type="button"  href="index.php"> Retour à la page d'accueil</a>
+  <div style="align-text:center;">
+		<h2>  <?php echo $message; ?>   </h2>
+    <a   class="buttonVaisselle" type="button"  href="index.php"> Retour à la page d'accueil</a>
+  </div>
 </body>
 
 
