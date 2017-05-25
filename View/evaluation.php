7515aeb4739bdbf8fdff71d9064fd06f2c75dcc1
Achraf <achrafelmahmoudi@gmail.com>
2017-05-25 13:32:39 +0200

    up

--- View/evaluation.php
+++ View/evaluation.php
@@ -5,13 +5,13 @@
     <form method="post" action="../Controller/controller_evaluation.php">
 
       AVIS SUR CE MEMBRE :
-      <input type="radio" name= "note" value="Positif"> Positif <br>
-      <input  type="radio" name="note" value="Neutre" > Neutre <br>
-      <input  type="radio" name="note" value="Negatif" > Négatif <br>
+      <input type="radio" name= "note" value="POSITIVE"> Positif <br>
+      <input  type="radio" name="note" value="NEUTRE" > Neutre <br>
+      <input  type="radio" name="note" value="NEGATIF" > Négatif <br>
 
   <span>Commentaire</span><textarea name="message" class="textarea-field"></textarea></label>
   <input type="hidden" name="pseudo" value=<?php echo $membre['pseudo']; ?> />
-  
+
 			<input  class="buttonVaisselle" type="submit" value="Notez " class="connectionButton"><br>
 
 		</form>
