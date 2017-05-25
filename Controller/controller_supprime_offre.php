631020ad0fa6d5a4439dfe0b625139ce0ce2507f
Achraf <achrafelmahmoudi@gmail.com>
2017-05-25 16:12:26 +0200

    up

--- Controller/controller_supprime_offre.php
+++ Controller/controller_supprime_offre.php
@@ -3,8 +3,11 @@
 
 require_once("Model/offre.php");
 require_once("Model/membre.php");
+echo "yeay";
 $idprofil= recupIdMembre($pseudo);
+echo "yeay2";
   supprimeOffreDuMembre($idprofil);
+  echo "yeay3";
  header("Location: ../validation.php?message=Vous_avez_bien_supprimé_cette_offre");
 
 ?>
